c23aff72f47bca1c0eaa4aa7b2e01ba416d2d1b4
Gor Manukyan <gorman93@mail.ru>
2019-02-16 19:23:08 +0400

    app.js

--- agatae/src/components/App.js
+++ agatae/src/components/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Lavaguynner} from './Lavaguynner/Lavaguynner';
+import {Navbar} from './Navbar/Navbar';
+import {Teacher_page} from './Teacher_page/Teacher_page';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar />
+		<Teacher_page />
+		<Lavaguynner />
       </div>
     );
   }
