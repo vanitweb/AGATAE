cc3f00b31d582d53524a83c73fef5c83c5c21af1
Gor Manukyan <gorman93@mail.ru>
2019-02-16 19:59:00 +0400

    offf esim ee

--- agatae/src/components/App.js
+++ agatae/src/components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import {Lavaguynner} from './Lavaguynner/Lavaguynner';
-import {Navbar} from './Navbar/Navbar';
+/*import {Navbar} from './Navbar/Navbar';*/
 import {TeacherPage} from './TeacherPage/TeacherPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
 		<TeacherPage />
 		<Lavaguynner />
       </div>
--- agatae/src/stores/UIStore.js
+++ agatae/src/stores/UIStore.js
@@ -2,7 +2,18 @@ import {teachers} from '../data/data';
 import {extendObservable, action} from 'mobx';
 
 class UIStore {
-	
+	appProps = {
+		_teachers: [],
+	};
+	constructor() {
+		extendObservable(this, this.appProps);
+	}
+	getTeachers() {
+		this.teachers = teachers;
+	}
+	set teachers(teachers) {
+		this._teachers = teachers;
+	}
 }
 
 export {UIStore};
