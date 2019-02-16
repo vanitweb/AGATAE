276dbf386933cf18848961ec2ace1e21b696a3cc
Gor Manukyan <gorman93@mail.ru>
2019-02-16 19:48:39 +0400

    hhh

--- agatae/src/components/App.js
+++ agatae/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Lavaguynner} from './Lavaguynner/Lavaguynner';
 import {Navbar} from './Navbar/Navbar';
-import {TeacherPage} from './Teacher_page/TeacherPage';
+import {TeacherPage} from './TeacherPage/TeacherPage';
 
 class App extends Component {
   render() {
--- agatae/src/index.js
+++ agatae/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../assets/styles/index.css';
+/*import '../assets/styles/index.css';*/
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
