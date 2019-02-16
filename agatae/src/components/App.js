f1ebdb7404dfbe9d123de8564e4430eda8650a71
Garik71426 <garo1997@mail.ru>
2019-02-16 20:08:01 +0400

    app.js

--- agatae/src/components/App.js
+++ agatae/src/components/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import {Lavaguynner} from './Lavaguynner/Lavaguynner';
 /*import {Navbar} from './Navbar/Navbar';*/
 import {TeacherPage} from './TeacherPage/TeacherPage';
+import {Slider} from './Slider/Slider';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+		<Slider />
 		<TeacherPage />
 		<Lavaguynner />
       </div>
