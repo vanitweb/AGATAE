ca9caf87378f79168d0ea291bd68d06c09f88509
Tukhik Gharagyozyan <Tukhik_k@mail.ru>
2019-02-16 20:43:53 +0400

    update Navbar

--- agatae/src/components/App.js
+++ agatae/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Lavaguynner} from './Lavaguynner/Lavaguynner';
-/*import {Navbar} from './Navbar/Navbar';*/
+import {Navbar} from './Navbar/Navbar';
 import {TeacherPage} from './TeacherPage/TeacherPage';
 import {Slider} from './Slider/Slider';
 
@@ -8,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+		<Navbar />
 		<Slider />
 		<TeacherPage />
 		<Lavaguynner />
--- agatae/src/components/Navbar/Navbar.jsx
+++ agatae/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import s from './Navbar.module.css';
-import {NavLink} from 'react-router-dom'
+
 
 const Navbar = () => {
-	return <nav className={s.nav}>
-        <div className={s.item}>
-          <NavLink to='/art' activeClassName={s.activeLink}>Արվեստ</NavLink>
-        </div>
-        <div className={`${s.item} ${s.active}`}>
-			<NavLink to='/sport' activeClassName={s.activeLink}>Սպորտ</NavLink>
-        </div>
-        <div className={s.item}>
-        <NavLink to='/science' activeClassName={s.activeLink}> Գիտություն</NavLink>
-        </div>
-        <div className={s.item}>
-        <NavLink to='/ather' activeClassName={s.activeLink}>Այլ</NavLink>
-        </div>
-        <div className={s.item}>
-        <NavLink to='/aboutUs' activeClassName={s.activeLink} className={s.item}>Մեր մասին</NavLink>
-        </div>
+	return <nav >
+          <div >Արվեստ</div>
+		  <div >Սպորտ</div>
+          <div >Գիտություն</div>
+		  <div >Այլ</div>
+          <div >Մեր մասին</div> 
        </nav>
 }
 export {Navbar};
