16393658f5ceb4eb44f5402c8660e882bc531f80
Arevik111 <arevik-elen@mail.ru>
2019-02-18 18:09:23 +0400

    Changes in Lavaguynner

---
+++ agatae/src/components/Lavaguynner/Cards.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Card, Button, CardImg, CardTitle, CardDeck, CardSubtitle, CardBody } from 'reactstrap';
+
+
+class Cards extends Component {
+
+    render() {
+		const{arrayOfLavTeachers} = this.props;
+        return(
+            <CardDeck>
+                {arrayOfLavTeachers.map((item,i) => {
+                    return (
+						<Card key={i}>
+							<CardImg top width="100%" src="arrayOfLavTeachers[i].photo"/>
+							<CardBody key={i}>
+								<CardTitle key={i}>{arrayOfLavTeachers[i].name}</CardTitle>
+								<CardSubtitle key={i}>{arrayOfLavTeachers[i].subject}</CardSubtitle>
+
+								<Button key={i} color="info">{arrayOfLavTeachers[i].aboutMe}</Button>
+							</CardBody>
+						</Card>
+					)
+                })}
+            </CardDeck>
+        );
+    }
+}
+
+export {Cards};
