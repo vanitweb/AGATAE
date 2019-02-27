e7a62ada0e16de3895307709282fc85d2c45dee0
Gor Manukyan <gorman93@mail.ru>
2019-02-28 00:25:53 +0400

    AppStore.js

--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -6,7 +6,9 @@ class AppStore {
     storeValues = {
         _teachers: [],
         _teacherName: '',
-        _subjectName: 'Հայերեն'
+        _subjectName: 'Մաթեմատիկա',
+		_searchValue: '',
+		_currentLink: ''
     };	
     constructor() {
         extendObservable(this, this.storeValues);
@@ -19,12 +21,19 @@ class AppStore {
         return this._teachers.filter(item => (item.name.substring(0, this._teacherName.length) === this._teacherName));
     }
     @computed get subjectData() {
-        return this._teachers.filter(item => (item.subject === this._subjectName));
+        return this._teachers.filter(item => (item.subject === this._subjectName || item.name === this._searchValue));
     }
     @computed get bestTeachers() {
         let tempTeachersArray = [...this._teachers];
         return (tempTeachersArray.sort((a,b) => (a.raiting > b.raiting) ? 1 : ((b.raiting > a.raiting) ? -1 : 0)).slice(0, 8));
     }
+	@computed get currentTeachers() {
+		if(this._currentLink === '/') {
+			return this.bestTeachers;
+		} else {
+			return this.subjectData;
+		}
+	}
     set teacherName(teacher) {
         this._teacherName = teacher;
     }
