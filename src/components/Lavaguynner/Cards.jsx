2dc82e778a87a7ea6ed2ffc73c2b0ea005e4f94e
Arevik111 <arevik-elen@mail.ru>
2019-02-28 00:23:09 +0400

    changes lavaguynner

--- src/components/Lavaguynner/Cards.jsx
+++ src/components/Lavaguynner/Cards.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
-import './Lavaguynner.css';
+import s from'./Lavaguynner.module.css';
 import { Container, Row, Col } from 'reactstrap';
 import { 
     Card,
@@ -18,19 +18,24 @@ class Cards extends Component {
     static contextTypes = {
         appStore: PropTypes.object.isRequired
     };
+	
+	/*onClick = (event) => {
+		this.context.appStore.currentTarget = event.currentTarget.getAttribute('data-option')
+	}*/
+	
     render() {
         const {bestTeachers} = this.context.appStore;
         return(
             <CardDeck>
                 {bestTeachers.map((item,i) => {
                     return (
-                        <Col sm={6} md={4} lg={3} className="pt-4">
-                            <Card key={i} style={{backgroundColor:'#FFFFD2'}}>
-                                <CardImg top width="100%" src={bestTeachers[i].photo}/>
-                                <CardBody key={i} >
-                                    <CardTitle key={i}>{bestTeachers[i].name}</CardTitle>
-                                    <CardSubtitle key={i}>{bestTeachers[i].subject}</CardSubtitle>
-                                    <Button key={i} color="success" >{bestTeachers[i].aboutMe}</Button>
+                        <Col key={i} sm={6} md={4} lg={3} className="pt-4">
+                            <Card style={{backgroundColor:'#FFFFD2'}}>
+                                <CardImg top width="100%" src={item.photo}/>
+                                <CardBody >
+                                    <CardTitle className={s.font}>{item.name}</CardTitle>
+                                    <CardSubtitle className={s.font}>{item.subject}</CardSubtitle>
+                                    <Button data-option={i} onClick={this.onClick} color="success" >{item.aboutMe}</Button>
                                 </CardBody>
                             </Card>
                         </Col>
