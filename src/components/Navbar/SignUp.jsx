aad83763ed3af35592205b201a74d7a6a1c997ab
Edgar Nikoghosyan <nikogosjanedgar@gmail.com>
2019-02-28 14:14:56 +0400

    footer fontAwesome add

--- src/components/Navbar/SignUp.jsx
+++ src/components/Navbar/SignUp.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import {NavItem,NavLink,Button,Input,Container} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 class SignUp extends Component {
     render() {
         return (
             <div>
-                <a href="/register">Register</a>
+             <NavLink variant="success" to="/register" tag={Link}>
+               <Button>Register</Button>
+              </NavLink>
             </div>
         );
     }
