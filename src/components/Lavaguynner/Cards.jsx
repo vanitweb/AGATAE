c411bdc66eb894c920a6e25b95c0921bb241c43f
Arevik111 <arevik-elen@mail.ru>
2019-03-01 14:42:48 +0400

    Add grafik

--- src/components/Lavaguynner/Cards.jsx
+++ src/components/Lavaguynner/Cards.jsx
@@ -34,7 +34,7 @@ class Cards extends Component {
                     return (
                         <Col sm={6} md={4} lg={3} className="pt-4">
                             <Card key={i} style={{backgroundColor:'#FFFFD2'}}>
-                                <CardImg top width="100%" src={item.photo}/>
+                                <CardImg top width="100%" height="130px" src={item.photo}/>
                                 <CardBody className="text-center">
                                     <CardTitle className={s.font}>{item.name}</CardTitle>
                                     <CardSubtitle className={s.font}>{item.subject}</CardSubtitle>
