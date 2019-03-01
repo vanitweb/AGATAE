bc0618d073bc6ab7911d8a8a853d459a3933dabc
Edgar Nikoghosyan <nikogosjanedgar@gmail.com>
2019-03-02 00:11:40 +0400

    add iccons on footer

--- src/components/Footer/FontIcons.jsx
+++ src/components/Footer/FontIcons.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ftrStyle from '../../../assets/styles/Footer/footer.module.css';
 
 class FontIcons extends Component {
     render() {
         return (
         	<Container>
         		<Row>
-        			<Col>
-        				 <FontAwesomeIcon icon="igloo" />
-        				 <FontAwesomeIcon icon="igloo" />
-        				 <FontAwesomeIcon icon="igloo" />
-        				 <FontAwesomeIcon icon="igloo" />
-        				 <FontAwesomeIcon icon="igloo" />
+        			<Col className={ftrStyle.icons}>
+        				 <FontAwesomeIcon icon="igloo" className={ftrStyle.item} />
+        				 <FontAwesomeIcon icon="anchor" className={ftrStyle.item} />
+        				 <FontAwesomeIcon icon="plane" className={ftrStyle.item} />
+        				 <FontAwesomeIcon icon="ad" className={ftrStyle.item} />
+        				 <FontAwesomeIcon icon="feather-alt" className={ftrStyle.item} />
         			</Col>
         		</Row>
         	</Container>
