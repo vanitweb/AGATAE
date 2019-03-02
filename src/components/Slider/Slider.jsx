1cdbc7f1de1ed3c41ec83dd1ad9a4adb8b71f201
AApinyan <arik200185@mail.ru>
2019-03-02 23:11:32 +0400

    items

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -7,41 +7,7 @@ import {
     CarouselIndicators,
     CarouselCaption
 } from 'reactstrap';
-import education from '../../../assets/images/Slider/education.jpg';
-import dance from '../../../assets/images/Slider/dance.jpg';
-import knitting from '../../../assets/images/Slider/knitting.jpg';
-import painting from '../../../assets/images/Slider/painting.jpg';
-import sport from '../../../assets/images/Slider/sport.jpg';
-
-
-const items = [
-    {
-        src: education,
-        altText: 'Կրթություն',
-        caption: 'Կրթություն'
-    },
-    {
-        src: dance,
-        altText: 'Արվեստ',
-        caption: 'Արվեստ'
-    },
-    {
-        src: knitting,
-        altText: 'Այլ',
-        caption: 'Այլ'
-    },
-    {
-        src: painting,
-        altText: 'Արվեստ',
-        caption: 'Արվեստ'
-    },
-    {
-        src: sport,
-        altText: 'Սպորտ',
-        caption: 'Սպորտ'
-
-    }
-];
+import {items} from './items';
 
 class Slider extends Component {
     constructor(props) {
