3f714ea4d266b415db3fe8f5e2a1b146bd7a5c32
Edgar Nikoghosyan <nikogosjanedgar@gmail.com>
2019-03-05 11:46:47 +0400

    error page fix

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
-import classes1 from '../../assets/styles/ErrorPage/ErrorPage.module.css';
+import errorPage from '../../assets/styles/ErrorPage/ErrorPage.module.css';
 import { Messages } from '../Messages';
 
 class ErrorPage extends Component {
     render() {
         return (
-            <Container className={classes1.errMessages}>
+            <Container className={errorPage.errMessages}>
             <Row className="justify-content-md-center">
             <Col>
                 {<h1>{Messages.forErrPage}</h1>}
